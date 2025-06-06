48ea75b0235f3019cb0a1c020d7030851bd62621
rohitsingh <rohitsingh986491@gmail.com>
2025-06-06 11:46:21 +0530

    fixed cors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://leetlab-backend-ifrk.onrender.com/"],
     credentials: true,
   })
 )
